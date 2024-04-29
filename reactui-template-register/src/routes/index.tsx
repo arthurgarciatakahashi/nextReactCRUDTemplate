279158bbb5d1195f3bf401d59413c98fe4871eff
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-04-29 01:33:36 -0300

    feat responsividade menu lateral

--- reactui-template-register/src/routes/index.tsx
+++ reactui-template-register/src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
+import { useAppThemeContext, useDrawerContext } from "../shared/contexts";
 
 
 export const AppRoutes = () => {
-    const { toggleTheme } = useAppThemeContext();
+    const { toggleDrawerOpen } = useDrawerContext();
 
     return (
         <Routes>
             <Route path="/pagina-inicial" element={<Button 
             variant="contained" 
             color="primary" 
-            onClick={toggleTheme}>Toggle Theme</Button>} />
+            onClick={toggleDrawerOpen}>Toggle Theme</Button>} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
--- reactui-template-register/src/shared/components/menu-lateral/MenuLateral.tsx
+++ reactui-template-register/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,17 +1,19 @@
-import { Avatar, Divider, Drawer, useTheme, List, ListItemButton, ListItemIcon, ListItemText, Icon } from "@mui/material";
+import { Avatar, Divider, Drawer, useTheme, List, ListItemButton, ListItemIcon, ListItemText, Icon, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateral {
     children: React.ReactNode;
 }
 export const MenuLateral: React.FC<IMenuLateral> = ({ children }) => {
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+    const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
 
     return (
         <>
-            <Drawer variant="permanent">
+            <Drawer open={isDrawerOpen} variant={smDown ? "temporary" : "permanent"} onClose={toggleDrawerOpen}>
                 <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column" >
                     <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
                         <Avatar
@@ -34,7 +36,7 @@ export const MenuLateral: React.FC<IMenuLateral> = ({ children }) => {
                     </Box>
                 </Box>
             </Drawer>
-            <Box height="100vh" marginLeft={theme.spacing(28)}>
+            <Box height="100vh" marginLeft={smDown ? "0" : theme.spacing(28)}>
                 {children}
             </Box>
         </>
