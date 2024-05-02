7b8c75b0d828c9dad9d05b8b7b9b1a5e429715df
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-05-02 19:33:29 -0300

    add listagem de cidades

--- src/pages/cidades/DetalheDeCidades.tsx
+++ src/pages/cidades/DetalheDeCidades.tsx
@@ -3,24 +3,20 @@ import { Box, Grid, LinearProgress, Paper, Typography } from '@mui/material';
 import { useNavigate, useParams } from 'react-router-dom';
 import * as yup from 'yup';
 
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { CidadesService } from '../../shared/services/api/cidades/CidadesService';
 import { VTextField, VForm, useVForm, IVFormErrors } from '../../shared/forms';
 import { FerramentasDeDetalhe } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 
-interface IFormData {
-  email: string;
-  cidadeId: number;
-  nomeCompleto: string;
+interface IFormData {  
+    nome: string;
 }
 const formValidationSchema: yup.SchemaOf<IFormData> = yup.object().shape({
-  cidadeId: yup.number().required(),
-  email: yup.string().required().email(),
-  nomeCompleto: yup.string().required().min(3),
+  nome: yup.string().required().min(3),
 });
 
-export const DetalheDePessoas: React.FC = () => {
+export const DetalheDeCidades: React.FC = () => {
   const { formRef, save, saveAndClose, isSaveAndClose } = useVForm();
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
@@ -33,23 +29,21 @@ export const DetalheDePessoas: React.FC = () => {
     if (id !== 'nova') {
       setIsLoading(true);
 
-      PessoasService.getById(Number(id))
+      CidadesService.getById(Number(id))
         .then((result) => {
           setIsLoading(false);
 
           if (result instanceof Error) {
             alert(result.message);
-            navigate('/pessoas');
+            navigate('/cidades');
           } else {
-            setNome(result.nomeCompleto);
+            setNome(result.nome);
             formRef.current?.setData(result);
           }
         });
     } else {
       formRef.current?.setData({
-        email: '',
-        cidadeId: '',
-        nomeCompleto: '',
+        nome: '',
       });
     }
   }, [id]);
@@ -62,7 +56,7 @@ export const DetalheDePessoas: React.FC = () => {
         setIsLoading(true);
 
         if (id === 'nova') {
-          PessoasService
+          CidadesService
             .create(dadosValidados)
             .then((result) => {
               setIsLoading(false);
@@ -71,14 +65,14 @@ export const DetalheDePessoas: React.FC = () => {
                 alert(result.message);
               } else {
                 if (isSaveAndClose()) {
-                  navigate('/pessoas');
+                  navigate('/cidades');
                 } else {
-                  navigate(`/pessoas/detalhe/${result}`);
+                  navigate(`/cidades/detalhe/${result}`);
                 }
               }
             });
         } else {
-          PessoasService
+          CidadesService
             .updateById(Number(id), { id: Number(id), ...dadosValidados })
             .then((result) => {
               setIsLoading(false);
@@ -87,7 +81,7 @@ export const DetalheDePessoas: React.FC = () => {
                 alert(result.message);
               } else {
                 if (isSaveAndClose()) {
-                  navigate('/pessoas');
+                  navigate('/cidades');
                 } else {
                   alert('registro alterado com sucesso.');
                 }
@@ -110,13 +104,13 @@ export const DetalheDePessoas: React.FC = () => {
 
   const handleDelete = (id: number) => {
     if (confirm('Realmente deseja apagar?')) {
-      PessoasService.deleteById(id)
+      CidadesService.deleteById(id)
         .then(result => {
           if (result instanceof Error) {
             alert(result.message);
           } else {
-            alert('Registro apagado com sucesso!');
-            navigate('/pessoas');
+            alert('Registro excluído com sucesso!');
+            navigate('/cidades');
           }
         });
     }
@@ -125,7 +119,7 @@ export const DetalheDePessoas: React.FC = () => {
 
   return (
     <LayoutBaseDePagina
-      titulo={id === 'nova' ? 'Nova pessoa' : nome}
+      titulo={id === 'nova' ? 'Nova cidade' : nome}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo='Nova'
@@ -135,9 +129,9 @@ export const DetalheDePessoas: React.FC = () => {
 
           aoClicarEmSalvar={save}
           aoClicarEmSalvarEFechar={saveAndClose}
-          aoClicarEmVoltar={() => navigate('/pessoas')}
+          aoClicarEmVoltar={() => navigate('/cidades')}
           aoClicarEmApagar={() => handleDelete(Number(id))}
-          aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
+          aoClicarEmNovo={() => navigate('/cidades/detalhe/nova')}
         />
       }
     >
@@ -160,36 +154,13 @@ export const DetalheDePessoas: React.FC = () => {
               <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
                 <VTextField
                   fullWidth
-                  name='nomeCompleto'
+                  name='nome'
                   disabled={isLoading}
-                  label='Nome completo'
+                  label='Nome'
                   onChange={e => setNome(e.target.value)}
                 />
               </Grid>
             </Grid>
-
-            <Grid container item direction="row" spacing={2}>
-              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
-                <VTextField
-                  fullWidth
-                  name='email'
-                  label='Email'
-                  disabled={isLoading}
-                />
-              </Grid>
-            </Grid>
-
-            <Grid container item direction="row" spacing={2}>
-              <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
-                <VTextField
-                  fullWidth
-                  label='Cidade'
-                  name='cidadeId'
-                  disabled={isLoading}
-                />
-              </Grid>
-            </Grid>
-
           </Grid>
 
         </Box>
